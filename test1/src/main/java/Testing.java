774dc1a7fc8ab8a98ac3801ebeed9348de2818aa
ilidosha <axesaspw10@gmail.com>
2018-10-17 16:05:49 +0500

    adding new branch

---
+++ test1/src/main/java/Testing.java
@@ -0,0 +1,8 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class Testing {
+    public static void Main(String[] argv) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
